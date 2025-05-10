f645c434b4360d886099b42f52b73bfd44bd6167
KangMyeongH <3044965@naver.com>
2025-05-10 17:11:01 +0900

    임시저장

--- CoreModule/include/RenderTarget.h
+++ CoreModule/include/RenderTarget.h
@@ -3,23 +3,39 @@
 
 namespace engine
 {
-    class RenderTarget : public Object
+    class COREMODULE_API RenderTarget : public Object
     {
         //======================================//
         //				constructor				//
         //======================================//
+    private:
+        RenderTarget();
+        ~RenderTarget() override = default;
+        RenderTarget(const RenderTarget& rhs);
 
         //======================================//
         //				 property				//
         //======================================//
 
+
+
         //======================================//
         //				  method				//
         //======================================//
+    public:
+        HRESULT Initialize(const ComPtr<ID3D11Device>& device, const ComPtr<ID3D11DeviceContext>& context, _uint sizeX, _uint sizeY, DXGI_FORMAT pixelFormat, const _float4& clearColor);
+
+
+    	static SharedPtr<RenderTarget> Create(const ComPtr<ID3D11Device>& device, const ComPtr<ID3D11DeviceContext>& context, _uint sizeX, _uint sizeY, DXGI_FORMAT pixelFormat, const _float4& clearColor);
+
+    	void Destroy() override {}
+
 
         //======================================//
         //				 serialize				//
         //======================================//
+
+
 
 
         //======================================//
@@ -29,5 +45,6 @@ namespace engine
         ComPtr<ID3D11Texture2D>     		m_Texture2D;
         ComPtr<ID3D11RenderTargetView> 		m_RTV;
         ComPtr<ID3D11ShaderResourceView>	m_SRV;
+        _float4                             m_ClearColor;
     };
 }
--- CoreModule/src/RenderTarget.cpp
+++ CoreModule/src/RenderTarget.cpp
@@ -1,1 +1,54 @@
 #include "RenderTarget.h"
+
+HRESULT engine::RenderTarget::Initialize(const ComPtr<ID3D11Device>& device, const ComPtr<ID3D11DeviceContext>& context,
+                                         const _uint sizeX, const _uint sizeY, const DXGI_FORMAT pixelFormat, const _float4& clearColor)
+{
+	D3D11_TEXTURE2D_DESC	texture2DDesc{};
+
+	texture2DDesc.Width = sizeX;
+	texture2DDesc.Height = sizeY;
+	texture2DDesc.MipLevels = 1;
+	texture2DDesc.ArraySize = 1;
+	texture2DDesc.Format = pixelFormat;
+
+	texture2DDesc.SampleDesc.Quality = 0;
+	texture2DDesc.SampleDesc.Count = 1;
+
+	texture2DDesc.Usage = D3D11_USAGE_DEFAULT;
+	texture2DDesc.BindFlags = D3D11_BIND_RENDER_TARGET | D3D11_BIND_SHADER_RESOURCE;
+	texture2DDesc.CPUAccessFlags = 0;
+	texture2DDesc.MiscFlags = 0;
+
+	if (FAILED(device->CreateTexture2D(&texture2DDesc, nullptr, m_Texture2D.ReleaseAndGetAddressOf())))
+	{
+		return E_FAIL;
+	}
+
+	if (FAILED(device->CreateRenderTargetView(m_Texture2D.Get(), nullptr, m_RTV.ReleaseAndGetAddressOf())))
+	{
+		return E_FAIL;
+	}
+
+	if (FAILED(device->CreateShaderResourceView(m_Texture2D.Get(), nullptr, m_SRV.ReleaseAndGetAddressOf())))
+	{
+		return E_FAIL;
+	}
+
+	m_ClearColor = clearColor;
+
+	return S_OK;
+}
+
+engine::SharedPtr<engine::RenderTarget> engine::RenderTarget::Create(const ComPtr<ID3D11Device>& device,
+                                                                     const ComPtr<ID3D11DeviceContext>& context, const _uint sizeX, const _uint sizeY, DXGI_FORMAT pixelFormat, const _float4& clearColor)
+{
+	SharedPtr<RenderTarget> renderTarget{ new RenderTarget(), [](const RenderTarget* ptr) { delete ptr; } };
+
+	if (FAILED(renderTarget->Initialize(device, context, sizeX, sizeY, pixelFormat, clearColor)))
+	{
+		std::cerr << "ERROR : Failed to Created : RenderTarget\n";
+		return nullptr;
+	}
+
+	return renderTarget;
+}
